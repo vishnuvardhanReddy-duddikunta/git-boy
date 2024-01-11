68673f2751506f6c393cbfedc0e020d05ee97175
vishnureddy-duddikunta <dvvrvishnureddy2@gmail.com>
2024-01-11 12:29:47 +0530

    added the impemention for interface

--- calsi.java
+++ calsi.java
@@ -3,3 +3,14 @@ interface calci
 public void getSum(int a,int b);
 public void getDiff(int a,int b);
 }
+
+class CalsiIMP implements calci{
+public void getSum(int a,int b){
+    retun a+b;
+}
+public void getDiff(int a,int b){
+    retun a-b;
+}
+
+
+}
