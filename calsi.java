28b8c42b0f42507f64f85c1bb73d1849aa8be6a9
vishnureddy-duddikunta <dvvrvishnureddy2@gmail.com>
2024-01-11 12:46:36 +0530

    added driver class

--- calsi.java
+++ calsi.java
@@ -1,4 +1,4 @@
-interface calci
+interface calsi
 {
 public void getSum(int a,int b);
 public void getDiff(int a,int b);
@@ -12,5 +12,19 @@ public void getDiff(int a,int b){
     retun a-b;
 }
 
+
+}
+import java.util.Scanner;
+class CalsiDriver{
+public static void main(String [] args)
+{
+Scanner sc=new Scanner(System.in);
+CalsiIMP i=new CalsiIMP();
+System.out.println("entre num1,num2")
+int a=sc.nextInt();
+int b=sc.nextInt();
+System.out.println(a+"+"+b+"="+i.getSum(a,b));
+System.out.println(a+"-"+b+"="+i.getDiff(a,b));
+}
 
 }
